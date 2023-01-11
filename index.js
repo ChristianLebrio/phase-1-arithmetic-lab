beb1b292a2056546cf977630efba56f23d8a4280
Christian Lebrio <ChristianLebrio@gmail.com>
2023-01-10 20:24:31 -0500

    solved tests

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let multiply;
+let num1 = 31;
+let num2 = 2;
+
+multiply = num1 * num2;
+
+let random;
+
+random = Math.floor(Math.random() * 10) + 1; 
+
+let mod;
+let num3 = 10;
+let num4 = 6;
+
+mod = num3 % num4;
+
+let max;
+
+max = Math.max (1, 2, 3 , 20)
